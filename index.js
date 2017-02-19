a5f1286edc54f8c9c6d67926681c39521128c326
darshilchapadia <darshil.chapadia@gmail.com>
2017-02-19 15:11:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello world!");
+var auth = require('http-auth');
+var basic = auth.basic({
+        realm: "DemoAuth"
+    }, (username, password, callback) => {              	
+		callback(username === "Darshil" && password === "test123");
+    }
+);
+
+app.use(auth.connect(basic));
+
+app.get('/me', function(req, res){	
+	setTimeout(function(){res.send({User : req.user, Delay : req.query.delay, Counter:req.query.counter});}, req.query.delay*1000);    		
 });
 
 var server = app.listen(3000, function () {
