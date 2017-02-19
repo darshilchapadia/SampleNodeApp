eb90181243d3b28bbf795b58f7feebd043e1b79c
darshilchapadia <darshil.chapadia@gmail.com>
2017-02-19 15:49:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var fileSystem = require('jsonfile');
+var jsonFile = 'counter.json'
 
 var auth = require('http-auth');
 var basic = auth.basic({
@@ -11,8 +13,31 @@ var basic = auth.basic({
 
 app.use(auth.connect(basic));
 
-app.get('/me', function(req, res){	
-	setTimeout(function(){res.send({User : req.user, Delay : req.query.delay, Counter:req.query.counter});}, req.query.delay*1000);    		
+var counter = 0;
+fileSystem.readFile(jsonFile,function(err,obj){
+		if(err)
+			console.log("Error",err);
+		else{		
+			counter = parseInt(obj.counter);
+		}
+	})
+
+app.get('/me', function(req, res){
+	
+	counter += parseInt(req.query.increment);	
+	setTimeout(function(){
+		res.send({
+			User : req.user, 
+			Delay : req.query.delay, 
+			Increment:counter
+		});
+	}, req.query.delay*1000);
+
+	var obj = {"counter" : counter};	
+	fileSystem.writeFile(jsonFile, obj,function(err){
+		if(err)
+			console.log("Error",err);				
+	});
 });
 
 var server = app.listen(3000, function () {
